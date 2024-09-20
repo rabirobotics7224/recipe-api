d57d5143f215228a0a69fb7f476d9b14691feb70
rabirobotics7224 <personalrobot9@gmail.com>
2024-09-20 21:10:35 +0530

    configured docker-config and checks for wait_for_db

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Return the sum of x and y"""
+    return x + y
+
+def subtract(x,y):
+    return x-y
---
+++ app/app/test.py
@@ -0,0 +1,19 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test cases for the calc module"""
+
+    def test_add_numbers(self):
+        res=calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        res=calc.subtract(9,1)
+        self.assertEqual(res,  8)
